81423fabd55e51750375124e91edaee7eed69c50
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-06-20 21:42:35 +0800

    duplicated part in ui

--- EmployeesManager.py
+++ EmployeesManager.py
@@ -9,16 +9,22 @@ class EmployeesManager:
 
     def add_employee(self):
 
-        print("\nEmployee Type")
-        print("1. Manager")
-        print("2. Developer")
-        print("3. Intern")
+        print("\n" + "-" * 40)
+        print("      ADD NEW EMPLOYEE")
+        print("-" * 40)
 
-        emp_type = input_is_valid("Choose type: ", 1, 3)
+        print("[1] Manager")
+        print("[2] Developer")
+        print("[3] Intern")
+
+        emp_type = input_is_valid(
+            "Select employee type: ",
+            1,
+            3
+        )
 
         name = input("Enter employee name: ")
         age = input_is_valid("Enter employee age: ")
-
         salary = input_is_valid("Enter salary/allowance: ")
 
         if emp_type == 1:
@@ -32,14 +38,27 @@ class EmployeesManager:
 
         self.employees.append(emp)
 
+        print("\n[SUCCESS] Employee added successfully!")
+
     def list_employee(self):
 
         if not self.employees:
             print("\nEmployee list is empty!")
             return
 
+        print("\n" + "=" * 70)
+        print(f"{'TYPE':<15}{'NAME':<20}{'AGE':<10}{'SALARY':<15}")
+        print("=" * 70)
+
         for emp in self.employees:
-            print(emp)
+            print(
+                f"{emp.__class__.__name__:<15}"
+                f"{emp.name:<20}"
+                f"{emp.age:<10}"
+                f"{emp.calculate_salary():<15}"
+            )
+
+        print("=" * 70)
 
     def delete_employees_with_age(self, age_from, age_to):
 
